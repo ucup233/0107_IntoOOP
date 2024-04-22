8348248c319d98a325627b71602c90d286d36c5a
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-04-22 15:29:28 +0700

    metode input mahasiswa

--- ObjectOrientedProgram/ObjectOrientedProgram.cpp
+++ ObjectOrientedProgram/ObjectOrientedProgram.cpp
@@ -6,4 +6,10 @@ public:
 	string nama;
 	int umur;
 	string jurusan;
+
+	void input() {
+		cout << "Nama : " << nama << endl;
+		cout << "Umur : " << umur << endl;
+		cout << "Jurusan : " << jurusan << endl;
+	}
 };
